44833041c7e4e90f97f79de9aac2f21ce36094c5
Daniel Bae <danibae91@icloud.com>
2021-05-03 23:24:01 -0700

    work in progress part one

--- app.py
+++ app.py
@@ -1,12 +1,75 @@
 """Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+# createdb blogly -> need to create database; \c blogly
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config['SECRET_KEY'] = 'chickenzarecool123'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+# define route for users
+@app.route('/')
+def home_page():
+    """Show home page of users"""
+    return redirect('/users')
+
+@app.route('/users')
+def show_users():
+    # instance of user 
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/users/new')
+def new_user_form():
+    return render_template('new.html')
+
+@app.route('/users/new', methods=["POST"])
+def new_user():
+    """Create new user"""
+    # form data 
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    # create new instance 
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    # add/commit to database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/')
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    """Show info about current user"""
+    # throw 404 error if ID not found 
+    user = User.query.get_or_404(user_id)
+    return render_template('details.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    """Show form to edit user details"""
+    # throw 404 error if ID not found 
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def confirm_edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
--- models.py
+++ models.py
@@ -1,1 +1,21 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy() 
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app) 
+
+# Models 
+class User(db.Model):
+    """User"""
+    __tablename__ = 'users' 
+
+    # define columns 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False)#default=DEFAULT_IMG_URL)
+
+
